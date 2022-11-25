611bed5d7a15247d7915da8fa6eb005f10475779
Jhenrique12 <jonhenrique007@gmail.com>
2022-11-25 10:54:19 -0300

    Update: change the logical order of the functions

--- script.js
+++ script.js
@@ -1,41 +1,50 @@
 
-document.querySelectorAll(".square").forEach(function (square) {
-  square.addEventListener("click", function () {
-    square.setAttribute('disabled', square.disabled)
-    
-    const squaresContainer = document.querySelector(".squaresContainer")
-
-    if(squaresContainer.dataset.value === "O") {
-      square.innerText = "O"
-      squaresContainer.dataset.value = "X"
-      square.classList.add("clicked-O")
-
-    }
-    else {
-      square.innerText = "X"
-      squaresContainer.dataset.value = "O"
-      square.classList.add("clicked-X")
-
-    }
-  })
-})
-
 const gameScreen = document.querySelector(".gameScreen")
 const menuScreen = document.querySelector(".menuScreen")
 
+const section = document.getElementById("players")
+  
+const namePlayer1 = document.getElementById("inputName1")
+const namePlayer2 = document.getElementById("inputName2")
+
 const btnStart = document.getElementById("btnStart").addEventListener("click", function () {
+
   gameScreen.style.setProperty("display", "flex")
   menuScreen.style.setProperty("display", "none")
-
-  const section = document.getElementById("players")
   
-  const namePlayer1 = document.getElementById("inputName1")
-  const namePlayer2 = document.getElementById("inputName2")
-
   const playersParagraph = document.querySelector("#players > p")
   playersParagraph.innerText = "Jogador 1: " + namePlayer1.value + "\nJogador 2: " + namePlayer2.value
   
-
+  currentPlayerParagraph.innerText = "Jogador da vez: " + namePlayer1.value
+  
   section.append(playersParagraph)
   
 })
+
+  const currentPlayerSection = document.getElementById("currentPlayer")
+
+  const currentPlayerParagraph = document.querySelector("#currentPlayer > p")
+
+  const square = document.querySelectorAll(".square").forEach(function (square) {
+    square.addEventListener("click", function () {
+      square.setAttribute('disabled', square.disabled)
+      const squaresContainer = document.querySelector(".squaresContainer")
+
+      if(squaresContainer.dataset.value === "O") {
+        square.innerText = "O"
+        squaresContainer.dataset.value = "X"
+        square.classList.add("clicked-O")
+
+        currentPlayerParagraph.innerText = "Jogador da vez: " + namePlayer1.value
+      }
+      else {
+        square.innerText = "X"
+        squaresContainer.dataset.value = "O"
+        square.classList.add("clicked-X")
+        
+        currentPlayerParagraph.innerText = "Jogador da vez: " + namePlayer2.value
+      }
+    })
+  })
+
+  // chek if win or lose and draw -> disabled and dataset.id or id= (possibilidades)  
